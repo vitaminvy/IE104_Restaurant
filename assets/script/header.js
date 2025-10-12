19eee98dbe174a526a7f005a1d0bfb2bceb529e3
vanthinh26102005 <vangvanminh.261005@gmail.com>
2025-10-12 12:36:55 +0700

    feat: update header effect and fix ui issue

--- assets/script/header.js
+++ assets/script/header.js
@@ -2,22 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.querySelector(".header__menu-btn");
   const nav = document.querySelector(".header__nav");
 
-  // Toggle khi click vào icon
+  // tooggle menu
   menuBtn.addEventListener("click", () => {
     nav.classList.toggle("header__nav--open");
   });
 
-  // Click ra ngoài (nếu click vào vùng nền tối)
+  // click outside to quit menu
   nav.addEventListener("click", (e) => {
     if (e.target === nav) {
       nav.classList.remove("header__nav--open");
     }
   });
 
-  // Khi resize > desktop thì đóng
+  // when resize -> close menu (in desktop mode)
   window.addEventListener("resize", () => {
     if (window.innerWidth >= 1024) {
       nav.classList.remove("header__nav--open");
     }
   });
 });
+
+// chnage header style when scroll
+window.addEventListener("scroll", () => {
+  const header = document.getElementById("header")
+
+  if (window.scrollY > 50) {
+    header.classList.add("header--scrolled")
+  } else {
+    header.classList.remove("header--scrolled")
+  }
+})

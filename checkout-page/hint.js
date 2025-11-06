904dabf67fae0847023d329b8d6cb1762219deaf
vanthinh26102005 <vangvanminh.261005@gmail.com>
2025-11-06 15:45:16 +0700

    update comment

--- checkout-page/hint.js
+++ checkout-page/hint.js
@@ -1,15 +1,15 @@
-// Logic: hiện hint của phương thức thanh toán
+// Logic: show hint of selected payment method
 document.addEventListener("DOMContentLoaded", () => {
   const radios = document.querySelectorAll(".payment__radio");
 
   radios.forEach((radio) => {
     radio.addEventListener("change", () => {
-      // Ẩn toàn bộ các hint trước
+      // hide all previous hints
       document
         .querySelectorAll(".payment__hint")
         .forEach((hint) => (hint.style.display = "none"));
 
-      // Hiển thị hint tương ứng với radio được chọn
+      // show hint of selected option
       const option = radio.closest(".payment__option");
       const hint = option?.querySelector(".payment__hint");
       if (hint) hint.style.display = "block";

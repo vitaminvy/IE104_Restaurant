e4aa70d40fb52610feb18de6173b951f16479bc8
huutri <huutri2701@gmail.com>
2025-11-13 15:47:34 +0700

    feat: btn back top top

--- assets/script/include-partials.js
+++ assets/script/include-partials.js
@@ -1,15 +1,25 @@
-// scripts / include - partials.js
+// scripts/include-partials.js
 (async () => {
-    const anchors = document.querySelectorAll('[data-include]');
-    for (const a of anchors) {
-        const url = a.getAttribute('data-include');
-        try {
-            const res = await fetch(url, { cache: 'no-cache' });
-            if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-            const html = await res.text();
-            a.outerHTML = html;
-        } catch (err) {
-            console.error('Include failed:', url, err);
-        }
+  const anchors = document.querySelectorAll("[data-include]");
+  let loaded = 0;
+  const total = anchors.length;
+
+  for (const a of anchors) {
+    const url = a.getAttribute("data-include");
+    try {
+      const res = await fetch(url, { cache: "no-cache" });
+      if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+
+      const html = await res.text();
+      a.outerHTML = html;
+    } catch (err) {
+      console.error("Include failed:", url, err);
+    } finally {
+      loaded++;
+      // create new signal for events
+      if (loaded === total) {
+        document.dispatchEvent(new Event("includes-loaded"));
+      }
     }
+  }
 })();
